6c5b577441656224bbbe120f057b7cf1e0dacc7b
Josh <peterson.joshua.j@gmail.com>
2017-03-25 23:28:32 -0400

    Added upload test cases and Setup blank controller in API

---
+++ FinanceIsoSwiftAPI/FinanceIsoSwiftAPI/Controllers/VSchemaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FinanceIsoSwiftAPI.Controllers
+{
+    public class VSchemaController : ApiController
+    {
+        public Boolean Get(string xml, string xsd)
+        {
+
+
+            return true;
+        }
+
+    }
+}
