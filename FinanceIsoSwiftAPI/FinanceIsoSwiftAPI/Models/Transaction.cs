342119dd1c4570bac73cfdaae6153952df269627
Josh <peterson.joshua.j@gmail.com>
2017-03-25 23:08:23 -0400

    Added empty base for website and began abstraction for the transaction data

---
+++ FinanceIsoSwiftAPI/FinanceIsoSwiftAPI/Models/Transaction.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinanceIsoSwiftAPI.Models
+{
+    public class Transaction
+    {
+        public DateTime dateTime { get; set; }
+        public Debtor debtor { get; set; }
+        public Creditor creditor { get; set; }
+        public string currency { get; set; }
+
+    }
+
+    public class Debtor
+    {
+        public string name { get; set; }
+        public string country { get; set; }
+        public string IBAN { get; set; }
+        public string bankName { get; set; }
+
+    }
+
+    public class Creditor
+    {
+        public string name { get; set; }
+        public string country { get; set; }
+        public string IBAN { get; set; }
+        public string bankName { get; set; }
+        public string street { get; set; }
+        public string addressNum { get; set; }
+        public string city { get; set; }
+        public string zipcode { get; set; }
+    }
+
+}
---
+++ FinanceIsoSwiftAPI/FinanceIsoSwiftAPI/Models/XMLSchemaValidation.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace FinanceIsoSwiftAPI.Models
+{
+    public class XMLSchemaValidation
+    {
+
+        public void ValidateXmlDocument(string xml, string schemaPath)
+        {
+            XmlReader documentToValidate = XmlReader.Create(new StringReader(xml));
+
+            XmlSchema schema;
+            using (var schemaReader = XmlReader.Create(schemaPath))
+            {
+                schema = XmlSchema.Read(schemaReader, ValidationEventHandler);
+            }
+
+            var schemas = new XmlSchemaSet();
+            schemas.Add(schema);
+
+            var settings = new XmlReaderSettings();
+            settings.ValidationType = ValidationType.Schema;
+            settings.Schemas = schemas;
+            settings.ValidationFlags =
+                XmlSchemaValidationFlags.ProcessIdentityConstraints |
+                XmlSchemaValidationFlags.ReportValidationWarnings;
+            settings.ValidationEventHandler += ValidationEventHandler;
+
+            using (var validationReader = XmlReader.Create(documentToValidate, settings))
+            {
+                while (validationReader.Read())
+                {
+                }
+            }
+        }
+
+        private static void ValidationEventHandler(
+            object sender, ValidationEventArgs args)
+        {
+            if (args.Severity == XmlSeverityType.Error)
+            {
+                throw args.Exception;
+            }
+
+            Debug.WriteLine(args.Message);
+        }
+
+
+    }
+}
