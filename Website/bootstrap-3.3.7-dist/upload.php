eecfea8666149033291da7d8104586464eb046db
Josh <peterson.joshua.j@gmail.com>
2017-03-26 14:00:49 -0400

    Final updates before demo

--- Website/bootstrap-3.3.7-dist/upload.php
+++ Website/bootstrap-3.3.7-dist/upload.php
@@ -1,26 +1,22 @@
 
 <?php
 
-$total = count($_FILES['input']['name']);
-
-// Loop through each file
-for($i=0; $i<$total; $i++) {
-  //Get the temp file path
-  $tmpFilePath = $_FILES['input']['tmp_name'][$i];
-
-  //Make sure we have a filepath
-  if ($tmpFilePath != ""){
-    //Setup our new file path
-    $newFilePath = "./uploads/" . $_FILES['input']['name'][$i];
-
-    //Upload the file into the temp dir
-    if(move_uploaded_file($tmpFilePath, $newFilePath)) {
-
-      //Handle other code here
-
-    }
-  }
-}
+session_start();
+    $target=$_POST['Uploads'];
+        if($target[strlen($target)-1]!='/')
+                $target=$target+'/';
+            $count=0;
+            foreach ($_FILES['input']['name'] as $filename) 
+            {
+                $temp=$target;
+                $tmp=$_FILES['input']['tmp_name'][$count];
+                $count=$count + 1;
+                $temp=$temp.basename($filename);
+                move_uploaded_file($tmp,$temp);
+                $temp='';
+                $tmp='';
+            }
+    header("financeisoswift.azurewebsites.net/upload.php");
 
 echo json_encode([0]};
 ?> 
